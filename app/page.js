b10256227ffb93d4011b7b90f39812e081c77115
kale <18kmkf@queensu.ca>
2025-04-22 11:28:55 -0400

    Load in my font

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-      <h1>My Home Page</h1>
+      <h1 className="font-sourceSerif text-2xl italic">My Home Page</h1>
     </div>
   );
 }
