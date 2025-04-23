5a0421d17d897335b3bbbd3b55ed3e8072aa7157
kale <18kmkf@queensu.ca>
2025-04-23 14:02:51 -0400

    compsa project page start

--- app/projects/compsa/page.js
+++ app/projects/compsa/page.js
@@ -8,15 +8,17 @@ export default function page() {
     >
       {/* Heading */}
       <div className="mb-10 text-center">
-        <h1 className="text-4xl italic font-source-serif">COMPSA</h1>
+        <h1 className="text-4xl italic font-source-serif">
+          COMPSA Admin Panel
+        </h1>
         <h3 className="text-base font-poppins text-center text-secondary-col">
-          May 2024 - April 2025
+          July 2023 - April 2024
         </h3>
       </div>
       {/* About project */}
       <div className="flex flex-col justify-center items-center">
         <Image
-          src="/qwic.png"
+          src="/compsa.png"
           width={700}
           height={200}
           alt="QWIC website picture"
@@ -25,45 +27,35 @@ export default function page() {
         <div className="grid grid-cols-4 grid-rows-4 gap-x-8 gap-y-4 w-4/6">
           <div className="row-span-1 col-span-1">
             <h2 className="font-bold mb-2">Timeline</h2>
-            <p>12 Months</p>
+            <p>4 Months</p>
             <div>
               <h2 className="font-bold mb-2 mt-8">Tools</h2>
               <ul>
                 <li>Next.js</li>
-                <li>Tailwind CSS</li>
-                <li>Vercel</li>
-                <li>Figma</li>
+                <li>Bulma CSS</li>
+                <li>Docker</li>
               </ul>
             </div>
           </div>
           <div className="col-span-3 row-span-1">
             <h2 className="font-bold mb-2">Overview</h2>
             <p>
-              I led a team of three developers using Agile methodologies to
-              build QWIC’s website. QWIC's original website was built using HTML
-              so the goal was to rebrand the website in a way that still fit the
-              current design identity, and use more modern technologies.
+              COMPSA's website featured pages such as events, members and job
+              postings. However, exec's didn't necessarily have the expertise to
+              edit the code everytime a change was needed on the website.
               <br />
               <br />
-              As part of the redesign process, I crafted the visual style, user
-              experience, and mobile/web designs using Figma. I met with club
-              exec's to gather requirements for each page, and iteratively
-              presented designs for feedback. <br />
+              This led to the development of an admin panel, where non-technical
+              users to add, edit or delete events, team members, and
+              content—automating website updates eliminating the need for club
+              members to make code changes.
               <br />
-              I developed the home page, sponsorships, mentorship and the
-              contact us pages using Next.js and Tailwind CSS and deploying the
-              website on Vercel.
-              <br />
-              <br />I provided 1:1 mentorship to web developers, holding
-              bi-weekly scrum meetings for task planning. Additionally I created
-              a development guide to support the team, ensuring consistency in
-              development practices.
             </p>
           </div>
 
           <div className="col-span-3 col-start-2 row-start-2">
             <h2 className="font-bold mb-2">My Role</h2>
-            <p>Team Lead, Mentor, Full-Stack Developer.</p>
+            <p>Frontend Developer</p>
           </div>
         </div>
       </div>
