14a2e888b207913a193aec2f6b16aa428f16f6db
kale <18kmkf@queensu.ca>
2025-04-23 14:10:10 -0400

    pentagram project page

--- app/projects/pentagram/page.js
+++ app/projects/pentagram/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function page() {
   return (
@@ -10,60 +11,56 @@ export default function page() {
       <div className="mb-10 text-center">
         <h1 className="text-4xl italic font-source-serif">The Pentagram</h1>
         <h3 className="text-base font-poppins text-center text-secondary-col">
-          May 2024 - April 2025
+          January 2023 - April 2023
         </h3>
       </div>
       {/* About project */}
       <div className="flex flex-col justify-center items-center">
         <Image
-          src="/qwic.png"
+          src="/pentagram.png"
           width={700}
           height={200}
-          alt="QWIC website picture"
+          alt="Pentagram video game screens"
           className="mb-10"
         />
-        <div className="grid grid-cols-4 grid-rows-4 gap-x-8 gap-y-4 w-4/6">
+        <div className="grid grid-cols-4 gap-x-8 gap-y-4 w-4/6">
           <div className="row-span-1 col-span-1">
             <h2 className="font-bold mb-2">Timeline</h2>
-            <p>12 Months</p>
+            <p>6 Months</p>
             <div>
               <h2 className="font-bold mb-2 mt-8">Tools</h2>
               <ul>
-                <li>Next.js</li>
-                <li>Tailwind CSS</li>
-                <li>Vercel</li>
-                <li>Figma</li>
+                <li>Procreate</li>
+                <li>Unity</li>
               </ul>
             </div>
           </div>
           <div className="col-span-3 row-span-1">
             <h2 className="font-bold mb-2">Overview</h2>
-            <p>
-              I led a team of three developers using Agile methodologies to
-              build QWIC’s website. QWIC's original website was built using HTML
-              so the goal was to rebrand the website in a way that still fit the
-              current design identity, and use more modern technologies.
+            <p className="mb-2">
+              The Pentagram is a detective game set on an old desktop computer.
+              Navigate the computer to find clues and solve mysteries to
+              discover what the secret organization, "The Pentagram," has been
+              up to and why they've been doing it.
               <br />
               <br />
-              As part of the redesign process, I crafted the visual style, user
-              experience, and mobile/web designs using Figma. I met with club
-              exec's to gather requirements for each page, and iteratively
-              presented designs for feedback. <br />
-              <br />
-              I developed the home page, sponsorships, mentorship and the
-              contact us pages using Next.js and Tailwind CSS and deploying the
-              website on Vercel.
-              <br />
-              <br />I provided 1:1 mentorship to web developers, holding
-              bi-weekly scrum meetings for task planning. Additionally I created
-              a development guide to support the team, ensuring consistency in
-              development practices.
+              This project was apart of my CISC226 (Video Game Design) semester
+              project. My primary role was creating the art assets, I created
+              all of the game’s pixel art, from the backgrounds to the icons on
+              the computer where our game is set. I also collaborated to
+              brainstorm the games storyline and puzzles.
             </p>
+            <Link
+              href="https://sampapais.itch.io/the-pentagram"
+              className="underline text-[#aaaaaa]"
+            >
+              Play our game for free!
+            </Link>
           </div>
 
           <div className="col-span-3 col-start-2 row-start-2">
             <h2 className="font-bold mb-2">My Role</h2>
-            <p>Team Lead, Mentor, Full-Stack Developer.</p>
+            <p>Artist, Story Developer.</p>
           </div>
         </div>
       </div>
