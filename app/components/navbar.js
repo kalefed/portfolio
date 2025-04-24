f68d7f5470dfd3f4c0fcf665887213391d8737ee
kale <18kmkf@queensu.ca>
2025-04-24 15:25:51 -0400

    small navbar fixes

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -4,9 +4,15 @@ export default function Navbar() {
     <div className="font-inter flex justify-between py-10">
       <p>kale</p>
       <div className="flex gap-4">
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/dashboard">Resume</Link>
+        <Link href="/" className="hover:underline">
+          Home
+        </Link>
+        <Link href="/about" className="hover:underline">
+          About
+        </Link>
+        <a href="mailto:kaleighfeder@gmail.com" className="hover:underline">
+          Contact Me
+        </a>
       </div>
     </div>
   );
