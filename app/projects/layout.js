4f9eb181e0b5b52abe3331a0e7edd95a2c76bc72
kale <18kmkf@queensu.ca>
2025-05-09 13:30:37 -0400

    Add back arrow

--- app/projects/layout.js
+++ app/projects/layout.js
@@ -1,5 +1,17 @@
 import FadeInAnimation from "../components/FadeInAnimation";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function ProjectLayout({ children }) {
-  return <FadeInAnimation>{children}</FadeInAnimation>;
+  return (
+    <FadeInAnimation>
+      <div className="my-4 sm:mx-20 md:mx-40 xl:mx-72">
+        <Link href="/" className="flex flex-row gap-2 items-center">
+          <Image src="/backArrow.svg" width={20} height={20} alt="Back arrow" />
+          <p className="hover:underline">Go back home</p>
+        </Link>
+      </div>
+      {children}
+    </FadeInAnimation>
+  );
 }
