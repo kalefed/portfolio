cc725f23b4d27d0a0f61ba613b699edab22d78dc
kale <18kmkf@queensu.ca>
2025-05-09 12:10:01 -0400

    Make project section mobile responsive

--- app/workingOn.js
+++ app/workingOn.js
@@ -5,14 +5,14 @@ export default function WorkingOn() {
   return (
     <div>
       <header className="mb-10">
-        <h1 className="font-source-serif text-6xl">
+        <h1 className="font-source-serif text-3xl md:text-4xl lg:text-6xl">
           what I've been <span className="italic">working</span> on ₊⟡⋆
         </h1>
-        <h3 className="font-inter text-xl pl-2 pt-5">
+        <h3 className="font-inter text-sm md:text-lg lg:text-xl pl-2 pt-5">
           click on a project to learn more
         </h3>
       </header>
-      <section className="grid grid-cols-2 gap-10">
+      <section className="grid grid-cols-1 lg:grid-cols-2 gap-5 lg:gap-10">
         <Link
           href="/projects/onmyshelf"
           className="p-4 hover:bg-neutral-100 hover:rounded-xl"
